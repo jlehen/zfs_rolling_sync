a3aa29b5dfe707e0fe8e745cc5ed21f94f296d7a
Jeremie Le Hen <jeremie@le-hen.org>
2013-10-17 20:01:29 +0200

    [zfs_rolling_sync] Let's configure through environment for now.

--- zfs_rolling_sync.sh
+++ zfs_rolling_sync.sh
@@ -1,30 +1,35 @@
 #!/bin/sh
 
-SNAPBASE='zfs_rolling_snap'
+: ${SNAPBASE:='zfs_rolling_snap'}
 SNAPNAME="$SNAPBASE"_`date +%Y%m%d-%H%M%S`
-HOST='finwe.chchile.org'
-DEST='tank'
-MAXSNAP=3
-V=
+: ${SRCHOST:='finwe.chchile.org'}
+: ${DESTFS:='tank'}
+: ${MAXSNAP:=3}
+: ${V:=}
+
+case "$V" in
+'') V= ;;
+*) V=-v ;;
+esac
 
 rsnaps=$(mktemp -t ${0##*/})		# Remote
 lsnaps=$(mktemp -t ${0##*/})		# Local
 commonsnaps=$(mktemp -t ${0##*/})	# Common
 trap "rm -f $rsnaps $lsnaps $commonsnaps" EXIT INT TERM
 
-ssh $HOST zfs list -Hrt snapshot -o name "$1" | grep "@${SNAPBASE}_" | \
+ssh $SRCHOST zfs list -Hrt snapshot -o name "$1" | grep "@${SNAPBASE}_" | \
     sed 's/.*@//' | sort -n > $rsnaps
 lastsnap=$(tail -n 1 $rsnaps)
 if [ -z "$lastsnap" ]; then
 	cat 1>&2 << EOF
 
 Snapshot on the remote host does not exist.  Please create it using
-ssh $HOST zfs snapshot -r $1@$SNAPNAME
+ssh $SRCHOST zfs snapshot -r $1@$SNAPNAME
 EOF
 	exit 1
 fi
 
-zfs list -Hrt snapshot -o name "$DEST/${1#*/}" | grep "@${SNAPBASE}_" | \
+zfs list -Hrt snapshot -o name "$DESTFS/${1#*/}" | grep "@${SNAPBASE}_" | \
     sed 's/.*@//' | sort -n > $lsnaps
 comm -12 $rsnaps $lsnaps > $commonsnaps
 lastsnap=$(tail -n 1 $commonsnaps)
@@ -32,18 +37,18 @@ if [ -z "$lastsnap" ]; then
 	cat 1>&2  << EOF
 
 Snapshot on this system does not exist.  Please transfer it using
-ssh $HOST zfs send -R $1@$lastsnap | zfs receive -d $DEST
+ssh $SRCHOST zfs send -R $1@$lastsnap | zfs receive -d $DESTFS
 EOF
 	exit 1
 fi
 
-ssh $HOST zfs snapshot -r "$1@$SNAPNAME"
-ssh $HOST zfs send $V -Ri "$1@$lastsnap" "$1@$SNAPNAME" | \
-    zfs receive $V -dF $DEST
+ssh $SRCHOST zfs snapshot -r "$1@$SNAPNAME"
+ssh $SRCHOST zfs send $V -Ri "$1@$lastsnap" "$1@$SNAPNAME" | \
+    zfs receive $V -dF $DESTFS
 
 snapcount=$(cat $commonsnaps | wc -l)
 [ $snapcount -le $MAXSNAP ] && exit
 for snap in $(head -n $(($snapcount - $MAXSNAP)) $commonsnaps); do
-	ssh $HOST zfs destroy $V -r "$1@$snap"
-	zfs destroy $V -r "$DEST/${1#*/}@$snap"
+	ssh $SRCHOST zfs destroy $V -r "$1@$snap"
+	zfs destroy $V -r "$DESTFS/${1#*/}@$snap"
 done
