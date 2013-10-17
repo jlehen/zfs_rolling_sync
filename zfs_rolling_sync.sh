472414e35d0efa678f4fe86b21af3867da58869f
Jeremie Le Hen <jeremie@le-hen.org>
2013-10-17 19:54:50 +0200

    [zfs_rolling_sync] Remove old common snapshots.

--- zfs_rolling_sync.sh
+++ zfs_rolling_sync.sh
@@ -4,10 +4,12 @@ SNAPBASE='zfs_rolling_snap'
 SNAPNAME="$SNAPBASE"_`date +%Y%m%d-%H%M%S`
 HOST='finwe.chchile.org'
 DEST='tank'
+MAXSNAP=3
 
-rsnaps=$(mktemp -t ${0##*/})
-lsnaps=$(mktemp -t ${0##*/})
-trap "rm -f $rsnaps $lsnaps" EXIT INT TERM
+rsnaps=$(mktemp -t ${0##*/})		# Remote
+lsnaps=$(mktemp -t ${0##*/})		# Local
+commonsnaps=$(mktemp -t ${0##*/})	# Common
+trap "rm -f $rsnaps $lsnaps $commonsnaps" EXIT INT TERM
 
 ssh $HOST zfs list -Hrt snapshot -o name "$1" | grep "@${SNAPBASE}_" | \
     sed 's/.*@//' | sort -n > $rsnaps
@@ -23,7 +25,8 @@ fi
 
 zfs list -Hrt snapshot -o name "$DEST/${1#*/}" | grep "@${SNAPBASE}_" | \
     sed 's/.*@//' | sort -n > $lsnaps
-lastsnap=$(comm -12 $rsnaps $lsnaps | tail -n 1)
+comm -12 $rsnaps $lsnaps > $commonsnaps
+lastsnap=$(tail -n 1 $commonsnaps)
 if [ -z "$lastsnap" ]; then
 	cat 1>&2  << EOF
 
@@ -35,3 +38,10 @@ fi
 
 ssh $HOST zfs snapshot -r "$1@$SNAPNAME"
 ssh $HOST zfs send -Ri "$1@$lastsnap" "$1@$SNAPNAME" | zfs receive -dF $DEST
+
+snapcount=$(cat $commonsnaps | wc -l)
+[ $snapcount -le $MAXSNAP ] && exit
+for snap in $(head -n $(($snapcount - $MAXSNAP)) $commonsnaps); do
+	ssh $HOST zfs destroy -r "$1@$snap"
+	zfs destroy -r "$DEST/${1#*/}@$snap"
+done
