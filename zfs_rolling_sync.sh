48bd7df24538fdcec2eaccbbf319cd1aa1b3c928
Jeremie Le Hen <jeremie@le-hen.org>
2013-11-12 23:37:31 +0100

    [zfs_rolling_sync] Add a better command-line interface and usage.

--- zfs_rolling_sync.sh
+++ zfs_rolling_sync.sh
@@ -1,11 +1,37 @@
 #!/bin/sh
 
-: ${SNAPBASE:='zfs_rolling_snap'}
-SNAPNAME="$SNAPBASE"_`date +%Y%m%d-%H%M%S`
-: ${SRCHOST:='finwe.chchile.org'}
-: ${DESTFS:='tank'}
+usage() {
+	[ $# -ne 0 ] && echo "Error: $*" >&2
+	cat >&2 << EOF
+Usage: ${0##*/} [-b snapname] [-m maxsnap] [-v] srchost::src/ds dst/ds
+Example: ${0##*/} peerhost::tank/jails/myfail tank
+EOF
+}
+
+: ${SNAPBASE:=zfs_rolling_sync-`hostname -s`}
 : ${MAXSNAP:=3}
 : ${V:=}
+
+while getopts 'b:m:v' opt; do
+	case "$opt" in
+	b) SNAPBASE="$OPTARG" ;;
+	m) MAXSNAP="$OPTARG" ;;
+	v) V="-v" ;;
+	esac
+done
+shift $(($OPTIND - 1))
+
+SNAPNAME="$SNAPBASE"_`date +%Y%m%d-%H%M%S`
+
+SRCHOST="${1%%::*}"
+SRCDS="${1##*::}"
+DESTDS="$2"
+
+case "$SRCHOST:$SRCDS:$DESTDS" in
+:*) usage "Missing source host" ;;
+*::*) usage "Missing source filesystem" ;;
+*:) usage "Missing destination filesystem" ;;
+esac
 
 case "$V" in
 '') V= ;;
@@ -17,38 +43,38 @@ lsnaps=$(mktemp -t ${0##*/})		# Local
 commonsnaps=$(mktemp -t ${0##*/})	# Common
 trap "rm -f $rsnaps $lsnaps $commonsnaps" EXIT INT TERM
 
-ssh $SRCHOST zfs list -Hrt snapshot -o name "$1" | grep "$1@${SNAPBASE}_" | \
-    sed 's/.*@//' | sort -n > $rsnaps
+ssh $SRCHOST zfs list -Hrt snapshot -o name "$SRCDS" | \
+    grep "$SRCDS@${SNAPBASE}_" | sed 's/.*@//' | sort -n > $rsnaps
 lastsnap=$(tail -n 1 $rsnaps)
 if [ -z "$lastsnap" ]; then
 	cat 1>&2 << EOF
 
 Snapshot on the remote host does not exist.  Please create it using
-ssh $SRCHOST zfs snapshot -r $1@$SNAPNAME
+ssh $SRCHOST zfs snapshot -r $SRCDS@$SNAPNAME
 EOF
 	exit 1
 fi
 
-zfs list -Hrt snapshot -o name "$DESTFS/${1#*/}" | \
-    grep "$DESTFS/${1#*/}@${SNAPBASE}_" | sed 's/.*@//' | sort -n > $lsnaps
+zfs list -Hrt snapshot -o name "$DESTDS/${SRCDS#*/}" | \
+    grep "$DESTDS/${SRCDS#*/}@${SNAPBASE}_" | sed 's/.*@//' | sort -n > $lsnaps
 comm -12 $rsnaps $lsnaps > $commonsnaps
 lastsnap=$(tail -n 1 $commonsnaps)
 if [ -z "$lastsnap" ]; then
 	cat 1>&2  << EOF
 
 Snapshot on this system does not exist.  Please transfer it using
-ssh $SRCHOST zfs send -R $1@$lastsnap | zfs receive -d $DESTFS
+ssh $SRCHOST zfs send -R $SRCDS@$lastsnap | zfs receive -d $DESTDS
 EOF
 	exit 1
 fi
 
-ssh $SRCHOST zfs snapshot -r "$1@$SNAPNAME"
-ssh $SRCHOST zfs send $V -Ri "$1@$lastsnap" "$1@$SNAPNAME" | \
-    zfs receive $V -dF $DESTFS
+ssh $SRCHOST zfs snapshot -r "$SRCDS@$SNAPNAME"
+ssh $SRCHOST zfs send $V -Ri "$SRCDS@$lastsnap" "$SRCDS@$SNAPNAME" | \
+    zfs receive $V -dF $DESTDS
 
 snapcount=$(cat $commonsnaps | wc -l)
 [ $snapcount -le $MAXSNAP ] && exit
 for snap in $(head -n $(($snapcount - $MAXSNAP)) $commonsnaps); do
-	ssh $SRCHOST zfs destroy $V -r "$1@$snap"
-	zfs destroy $V -r "$DESTFS/${1#*/}@$snap"
+	ssh $SRCHOST zfs destroy $V -r "$SRCDS@$snap"
+	zfs destroy $V -r "$DESTDS/${SRCDS#*/}@$snap"
 done
